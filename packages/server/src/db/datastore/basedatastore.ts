a1e7eddb6a3afa49a6773bdbef9600928a1857a6
Christy George <christy.george@gmail.com>
2017-05-04 22:24:19 +0530

    Minor Changes

--- packages/server/src/db/datastore/basedatastore.ts
+++ packages/server/src/db/datastore/basedatastore.ts
@@ -351,11 +351,11 @@ export abstract class BaseDatastore {
   }
 
   private async getFiltersWithTransaction(rc : RunContextServer, filters: {[index : string] : any}, transaction : any, ignoreRNF ?: boolean) : Promise<void> {
-    let query = this._datastore.createQuery(this.namespace, this.kindName)
+    let query = transaction.createQuery(this.namespace, this.kindName)
     filters.forEach ((value : any, key : string) => {
         query = query.filter (key, value)
     })
-    let res = await this._datastore.runQuery(query)
+    let res = await query.run ()
     const entityRec = res[0],
           key       = res[this._datastore.KEY].path   
 
