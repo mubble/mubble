371eb8edbc9404ff23adc0ce9dea75ddb3e74fa0
schnill <nishant0509@gmail.com>
2017-06-22 14:11:46 +0530

    for content moderation testing on staging

--- packages/server/src/xmn/http-xmn.ts
+++ packages/server/src/xmn/http-xmn.ts
@@ -12,14 +12,14 @@ import {RunContextServer}     from '../rc-server'
 
 export class HttpXmn {
 
-  constructor(private refRc: RunContextServer) {
+  constructor(private refRc: RunContextServer, private azure: any) {
 
   }
 
   requestHandler(req: http.IncomingMessage, res: http.ServerResponse) : void {
 
     const rc = this.refRc.copyConstruct('', 'HttpReq')
-
+    
     rc.isDebug() && rc.debug(rc.getName(this), 'http api', req.url, 
       req.headers, req.socket ? {localAddr: req.socket.localAddress  + ':' + req.socket.localPort, 
       remote: req.socket.remoteAddress + ':' + req.socket.remotePort } : 'no socket')
@@ -29,28 +29,34 @@ export class HttpXmn {
     req.on('data', function(data) {
       body += data
     })
+    
     const _ : HttpXmn = this
     req.on('end', function() {
       
-      _.processRequest(req.url as string , qs.parse(body))
+      _.processRequest(req.url, body)
       
       res.setHeader('Content-Type', 'text/html');
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('ok');
     })
-
+    
   }
 
-  processRequest(url: string, body: any) {
+  processRequest(url: string | undefined, body: any) {
 
     const rc = this.refRc.copyConstruct('', 'HttpReq')
+    
+    if (url == undefined) {
+      rc.isDebug() && rc.debug(rc.getName(this), '!!! url is undefined', url)
+      return
+    }
 
     if (url.endsWith('onJobCompletion')) {
-      //azure.postJobCompletion(rc, body) 
+      this.azure.postJobCompletion(rc, JSON.parse(body)) 
     } else if (url.endsWith('onReviewCompletion')) {
-      //azure.postReviewCompletion(rc, body)
+      this.azure.postReviewCompletion(rc, JSON.parse(body))
     } else {
-      rc.isDebug && rc.debug('unknown url', url)
+      rc.isDebug() && rc.debug(rc.getName(this), 'unknown url', url)
     }
   }
 }
