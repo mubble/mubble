1406d9ccc6023498be92b2b0120957a19fedc5fd
Gaurav Kulshreshtha <gaurav.kulshreshtha@mubble.in>
2017-09-01 15:51:00 +0530

    pubsub refactoring

---
+++ packages/server/src/gcp/pubsub/util.ts
@@ -0,0 +1,43 @@
+/*------------------------------------------------------------------------------
+   About      : Google Pub-Sub Messages Util Apis
+   
+   Created on : Fri Sep 01 2017
+   Author     : Gaurav Kulshreshtha
+   
+   Copyright (c) 2017 Mubble Networks Private Limited. All rights reserved.
+------------------------------------------------------------------------------*/
+import {set}                        from '@mubble/core'
+
+
+export type PubSubCbMsg = {
+  message : {
+    data          : string  // base 64 encoded data string
+    attributes    : object  
+    message_id    : string 
+    messageId     : string 
+    publish_time  : string 
+    publishTime   : string 
+  },
+  subscription : string  // full subscription name
+
+}
+
+export class PubSubMsgDecoder {
+
+  constructor(public msg : PubSubCbMsg){}
+
+  public getJsonData<T>() : T {
+    return JSON.parse(this.getStringData())
+  }
+
+  public getStringData() : string {
+    return new Buffer(this.msg.message.data , 'base64').toString()
+  }
+
+  public getPublishDate() : Date {
+    const date = new Date()
+    set(date , this.msg.message.publish_time , '%yyyy%-%mm%-%dd%T%hh%:%MM%:%ss%.%ms%Z' , 0 )
+    return date
+  }
+}
+
