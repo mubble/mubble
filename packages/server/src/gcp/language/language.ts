d7ab2682df0f85c0e7f048b1a7196059c2aa9422
Christy George <christy.george@gmail.com>
2017-09-14 10:13:05 +0530

    Language Changes

--- packages/server/src/gcp/language/language.ts
+++ packages/server/src/gcp/language/language.ts
@@ -9,8 +9,9 @@
 
 const language : any = require('@google-cloud/language')
 
-import {RunContextServer}    from '../../rc-server'
-import {GcloudEnv}           from '../../gcp/gcloud-env'
+import * as lo                                    from 'lodash'
+import {RunContextServer}                         from '../../rc-server'
+import {GcloudEnv}                                from '../../gcp/gcloud-env'
 
 export type GcpEntityInfo = {
   name          : string, 
@@ -59,16 +60,40 @@ export class GcpLanguageBase {
     return this.analyzeEntitiesInternal (rc, document)
   }
 
-  static async analyzeEntitiesInternal (rc : RunContextServer, document: object) {
+  // TODO: Can support threshold as an argument.
+  static async analyzeEntitiesInternal (rc : RunContextServer, document: any) : Promise<Array<GcpEntityInfo>> {
     const res      = await this._language.analyzeEntities ({document: document}) 
     rc.isDebug() && rc.debug (rc.getName (this), 'Entity Analysis [Text] => Language:', res[0].language, '/', res[0].entities.length, 'entries.')
     const entities = res[0].entities.map ((entityInfo: any) => {
       return { 
         name: entityInfo.name, type: entityInfo.type, language: res[0].language,
-        wikipedia_url: entityInfo.metadata && entityInfo.metadata.wikipedia_url || '',
+        wikipedia_url: entityInfo.metadata 
+                    && entityInfo.metadata.wikipedia_url 
+                    && entityInfo.metadata.wikipedia_url.replace ('https://en.wikipedia.org/wiki/', '') 
+                    || '',
         salience: entityInfo.salience, occurences: entityInfo.mentions.length
       } as GcpEntityInfo
     })
-    return entities
+    rc.isDebug() && this.findDuplicates (rc, entities)
+    // NOTE: There are duplicates in name with different salience value.. We take the first..
+    const uniqueEntities = lo.uniqBy (entities, 'name') as Array<GcpEntityInfo>
+    rc.isStatus() && rc.status (rc.getName (this), 'Unique Entries:', uniqueEntities.length, '/', entities.length)
+    return uniqueEntities
+  }
+
+  static findDuplicates (rc: RunContextServer, entities: Array<GcpEntityInfo>) {
+    const uniq = entities.map ((eInfo: GcpEntityInfo) => {
+      return { name: eInfo.name, count: 1, saliencies: [eInfo.salience], occurences: [eInfo.occurences] }
+    }).reduce ((accum: any, obj: any) => { 
+      const val = accum[obj.name] || { name: obj.name, count: 0, saliencies: [], occurences: []}
+      val.count = val.count + obj.count
+      val.saliencies.push (...obj.saliencies)
+      val.occurences.push (...obj.occurences)
+      accum[obj.name] = val
+      return accum
+    }, {})
+    const dups = Object.keys (uniq).filter ((a: string) => uniq[a].count > 1)
+    console.log ('\t==>Duplicates:', JSON.stringify (dups))
+
   }
 }
