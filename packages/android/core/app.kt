8531e7598affa1757702b4b9c24d63a0a15aa201
Raghvendra Varma <raghavv@gmail.com>
2017-11-23 18:12:42 +0530

    added android package

---
+++ packages/android/core/app.kt
@@ -0,0 +1,32 @@
+package `in`.mubble.android.core
+
+import android.app.Application
+
+/*------------------------------------------------------------------------------
+   About      : 
+   
+   Created on : 23/11/17
+   Author     : Raghvendra Varma
+   
+   Copyright (c) 2017 Mubble Networks Private Limited. All rights reserved.
+------------------------------------------------------------------------------*/
+
+object App {
+  val instance: Application by lazy {
+    MyApp.instance!!
+  }
+}
+
+class MyApp : Application() {
+
+  companion object {
+    internal var instance: Application? = null
+  }
+
+
+  override fun onCreate() {
+    super.onCreate()
+    instance = this
+  }
+
+}
---
+++ packages/android/ui/mubble-base-activity.kt
@@ -0,0 +1,34 @@
+package `in`.mubble.android.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/*------------------------------------------------------------------------------
+   About      : 
+   
+   Created on : 23/11/17
+   Author     : Raghvendra Varma
+   
+   Copyright (c) 2017 Mubble Networks Private Limited. All rights reserved.
+------------------------------------------------------------------------------*/
+
+
+abstract class MubbleBaseActivity: AppCompatActivity() {
+
+  final override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    onMubbleCreate(savedInstanceState, null)
+  }
+
+  final override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    super.onCreate(savedInstanceState, persistentState)
+    onMubbleCreate(savedInstanceState, persistentState)
+  }
+
+  open fun onMubbleCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+
+  }
+
+
+}
