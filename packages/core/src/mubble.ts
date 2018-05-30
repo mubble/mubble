3f689a80e59d33a1424f6d220dcbdea62dab0d27
Vishal Sinha <vishal.sinha@mubble.in>
2018-05-30 13:23:49 +0530

    execFn and delayedPromise functions added to Mubble.uPromise

--- packages/core/src/mubble.ts
+++ packages/core/src/mubble.ts
@@ -25,6 +25,43 @@ export namespace Mubble {
 
   export class uPromise<T> {
 
+    static execFn(fn: Function, context: Object | null, ...params: any[]) : Promise<any> {
+      const promiseFn = this.getFn(fn, context)
+      return promiseFn(...params)
+    }
+  
+    static delayedPromise<X>(ms: number, fulfillWith ?: X): Promise<X> {
+      return new Promise( function(resolve : any, reject : any) {
+        setTimeout(ms, function() {
+          resolve(fulfillWith)
+        })
+      })
+    }
+
+    private static getFn(fn: Function, context: Object | null): (...arParam: any[]) => Promise<any> {
+
+      return function(...arParam: any[]): Promise<any> {
+        
+        return new Promise( function(resolve : any, reject : any) {
+          
+          function cb(...arCbParam: any[]) {
+            
+            const err = arCbParam.shift()
+            if (err) return reject(err)
+            // Resolved with multiple values; this would actually give first value in promise
+            resolve.apply(null, arCbParam)
+          }
+          
+          try {
+            arParam.push(cb)
+            fn.apply(context, arParam)
+          } catch (e) {
+            reject(e)
+          }
+        })
+      }
+    }
+
     private  fnResolve : null | ((result: any) => any)
     private  fnReject  : null | ((err: Error)  => any)
     readonly promise   : Promise<T>
