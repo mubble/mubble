57d2778d875920702c5b4aa82c379f852225ff5c
adityabaddur <baditya@obopay.com>
2019-03-18 14:27:40 +0530

    refactored location strategu

--- packages/browser/src/ui/router/location-strategy.ts
+++ packages/browser/src/ui/router/location-strategy.ts
@@ -12,17 +12,32 @@
 import { Injectable, 
          Inject
        }                        from '@angular/core'
-import { HashLocationStrategy, 
-         PlatformLocation 
+import { LocationStrategy,
+         LocationChangeListener
        }                        from '@angular/common'
 import { RunContextBrowser }    from '@mubble/browser/rc-browser'
 
 @Injectable()
-export class AppLocationStrategy extends HashLocationStrategy {
+export class AppLocationStrategy extends LocationStrategy {
 
-  constructor(@Inject('RunContext') protected rc  : RunContextBrowser,
-              protected platformLocation          : PlatformLocation) {
-    super(platformLocation)
+  constructor(@Inject('RunContext') protected rc  : RunContextBrowser) {
+    super()
+  }
+
+  getBaseHref() {
+    return '.'
+  }
+
+  path() {
+    return location.pathname
+  }
+
+  prepareExternalUrl() {
+    return ''
+  }
+
+  onPopState(fn: LocationChangeListener) {
+    
   }
 
   pushState(state: any, title: string, path: string, queryParams: string) {
@@ -33,6 +48,14 @@ export class AppLocationStrategy extends HashLocationStrategy {
   replaceState(state: any, title: string, path: string, queryParams: string) {
     this.rc.isDebug() && this.rc.debug(this.rc.getName(this), `ignoring replace state
       ${state} , ${title}, ${path}, ${queryParams}`)
+  }
+
+  forward() {
+
+  }
+
+  back() {
+
   }
 
 }
