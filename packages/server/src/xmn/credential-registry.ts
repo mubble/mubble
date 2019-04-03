79120d7243c8fb5eaa6ae92ee9b39ef5b7610b65
Vishal Sinha <svishal@obopay.com>
2019-04-03 14:57:28 +0530

    registry changes (for some reason not merged)

--- packages/server/src/xmn/credential-registry.ts
+++ packages/server/src/xmn/credential-registry.ts
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------
-   About      : Sync Credentials and Credential Registry
+   About      : Credentials and Credential Registry
    
    Created on : Tue Feb 05 2019
    Author     : Vishal Sinha
@@ -7,7 +7,7 @@
    Copyright (c) 2019 Obopay Mobile Technologies Pvt Ltd. All rights reserved.
 ------------------------------------------------------------------------------*/
 
-export interface SyncCredentials {
+export interface ServerCredentials {
   id            : string             // Client / Server identifier
   syncHash      : string             // Client / Server public key
   host          : string             // Server host
@@ -16,5 +16,14 @@ export interface SyncCredentials {
 }
 
 export interface CredentialRegistry {
-  getCredential(id : string) : SyncCredentials
+  getCredential(id : string) : ServerCredentials | undefined
+}
+
+export interface AppCredentials {
+  appShortName : string           // App Identifier
+  permittedIps : Array<string>    // Client Permitted Ips
+}
+
+export interface AppRegistry {
+  getCredential(appShortName : string) : AppCredentials | undefined
 }
