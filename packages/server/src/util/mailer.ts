a5b360bb9a96ace3ff8710a1e9cfec75a12c047b
sanithaps <sanithaps@obopay.com>
2019-08-12 17:53:16 +0530

    mailer removed log stmt

--- packages/server/src/util/mailer.ts
+++ packages/server/src/util/mailer.ts
@@ -11,7 +11,7 @@ import * as nodemailer from 'nodemailer'
 export async function sendGMail(userId: string, password: string, from: string, addresses: string[], 
                                 subject: string, body: string) {
 
-console.log(`Test1`);
+//console.log(`Test1`);
 
 try {
 
@@ -25,17 +25,17 @@ try {
     }
   })
 
-  console.log(`Test2`);
+  //console.log(`Test2`);
   
-
-  const info = await transporter.sendMail({
-    from    : from, // sender address
-    to      : addresses.join(','), // list of receivers
-    subject : subject, // Subject line
-    html    : body, // plain text body
-  })
-
-  console.log(`Sent Email with messageId: ${info.messageId}`)
+  if(addresses && addresses.length>0){
+    const info = await transporter.sendMail({
+      from    : from, // sender address
+      to      : addresses.join(','), // list of receivers
+      subject : subject, // Subject line
+      html    : body, // plain text body
+    })
+  }
+  //console.log(`Sent Email with messageId: ${info.messageId}`)
 
 } catch (e) {
   throw e
