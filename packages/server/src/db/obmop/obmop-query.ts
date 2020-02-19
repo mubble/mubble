a3b0e3e53403649e7c8823097c0dea31adad824f
Vishal Sinha <svishal@obopay.com>
2020-02-19 11:20:31 +0530

    Changed QueryBuilder name to ObmopQueryBuilder

--- packages/server/src/db/obmop/obmop-query.ts
+++ packages/server/src/db/obmop/obmop-query.ts
@@ -12,14 +12,14 @@ import { ObmopBaseEntity }    from './obmop-base'
 import * as lo                from 'lodash'
 
 export type ObmopSeparator = 'AND' | 'OR'
-export type ObmopOperator  = '=' | '<' | '>' | '<=' | '>=' | 'IN' | 'LIKE' | 'NOT NULL' | '!=' | 'IS NULL'
+export type ObmopOperator  = '=' | '!=' | '<' | '>' | '<=' | '>=' | 'IN' | 'LIKE' | 'IS NULL' | 'NOT NULL'
 
 export type ObmopQueryCondition<T extends ObmopBaseEntity> = {
   queryStr : string
   binds    : Array<any>
 }
 
-export namespace QueryBuilder {
+export namespace ObmopQueryBuilder {
 
   /**
    * @param rc RunContext, used for logging.
@@ -28,20 +28,18 @@ export namespace QueryBuilder {
    * @param operator Conditional operator compatible with SQL databases. By default it is '='.
    * @param upper optional for case insensitive search. By default it is false.
    */
-
-  export function newCondition<T extends ObmopBaseEntity>(
-    rc       : RunContextServer, 
-    key      : keyof T, 
-    value    : any, 
-    operator : ObmopOperator = '=',
-    upper    : boolean       = false) : ObmopQueryCondition<T> {
+  export function newCondition<T extends ObmopBaseEntity>(rc        : RunContextServer, 
+                                                          key       : keyof T, 
+                                                          value    ?: any, 
+                                                          operator  : ObmopOperator = '=',
+                                                          upper     : boolean       = false) : ObmopQueryCondition<T> {
 
     rc.isDebug() && rc.debug(rc.getName(this), 'Creating new condition.', key, value, operator, upper)  
 
     const binds = [] as Array<any>,
           conds = [] as Array<any>
 
-      let queryString : string,
+      let queryStr : string,
           c           : number = 1
 
     if(value instanceof Array) {
@@ -50,36 +48,30 @@ export namespace QueryBuilder {
         binds.push(val)
       }
       
-      queryString = upper ? `(UPPER(${key}) ${operator} (${conds.join(', ')}))`
-                          : `(${key} ${operator} (${conds.join(', ')}))`
-    } else if(value === undefined) {
-        queryString = upper ? `(UPPER(${key}) ${operator})` 
-                            : `(${key} ${operator})`  
+      queryStr = upper ? `(UPPER(${key}) ${operator} (${conds.join(', ')}))`
+                       : `(${key} ${operator} (${conds.join(', ')}))`
+    } else if(value === undefined || value === null) {
+        queryStr = upper ? `(UPPER(${key}) ${operator})` 
+                         : `(${key} ${operator})`  
     } else {
       binds.push(value)
-      queryString = upper ? `(UPPER(${key}) ${operator} :${c++})` 
-                          : `(${key} ${operator} :${c++})`                   
+      queryStr = upper ? `(UPPER(${key}) ${operator} :${c++})` 
+                       : `(${key} ${operator} :${c++})`                   
     }
 
-    rc.isDebug() && rc.debug(rc.getName(this), 'newCondition', queryString, binds)
+    rc.isDebug() && rc.debug(rc.getName(this), 'newCondition', queryStr, binds)
 
-    return { 
-              queryStr : queryString, 
-              binds
-            }                              
+    return { queryStr, binds }                              
   }
-  
 
   /**
    * @param rc RunContext, used for logging.
    * @param conditions array of ObmopQueryCondition.
    * @param separator separator to join conditions.
    */
-
-  export function joinConditions<T extends ObmopBaseEntity>(
-    rc         : RunContextServer,
-    conditions : Array<ObmopQueryCondition<T>>,
-    separator  : ObmopSeparator) : ObmopQueryCondition<T> {
+  export function joinConditions<T extends ObmopBaseEntity>(rc         : RunContextServer,
+                                                            conditions : Array<ObmopQueryCondition<T>>,
+                                                            separator  : ObmopSeparator) : ObmopQueryCondition<T> {
 
     rc.isDebug() && rc.debug(rc.getName(this), 'joinConditons', conditions, separator)
 
@@ -89,17 +81,14 @@ export namespace QueryBuilder {
           binds        = lo.flattenDeep(lo.concat([], conditions.map(cond => cond.binds))),
           queryString  = `(${queryStrings.join(` ${separator} `)})`
 
-    const queryMap = queryString.split(' ').map(char => {
+    const queryStr = queryString.split(' ').map(char => {
       if(char.match(/:[0-9]+/) !== null) {
         count++
         return char.replace(/:[0-9]+/, `:${count}`)
       }
       return char
-    })
+    }).join(' ')
 
-    return {
-             queryStr : queryMap.join(' '),
-             binds
-    }
+    return { queryStr, binds }
   }
 }
