1820051d5f97b10001a08fc32fbf65050cd2cdde
Sapna-Kumari <sapna.kumari@obopay.com>
2020-05-19 21:38:09 +0530

    masking issue fix

--- packages/browser/src/ui/mu-components/directives/masking-value.directive.ts
+++ packages/browser/src/ui/mu-components/directives/masking-value.directive.ts
@@ -8,28 +8,49 @@ import { Directive,
 
 const KEY_UP  = 'keyup'
 
+export interface MaskingParams {
+  maxLength        : number
+  maskedLength     : number
+  maskWith        ?: string
+  startSkipCount  ?: number
+  endSkipCount    ?: number
+}
+
 @Directive({
   selector: '[maskingValue]'
 })
 
 export class MaskingValueDirective {
 
-  updatedString : string = ''
+  @Input('maskingValue') maskingParams : MaskingParams
 
-  @Input('maskedLength') maskedLength : number
-  @Input('maxStringLength') maxLength : number
-  @Output() value : EventEmitter<string> = new EventEmitter<string>()
+  @Output() maskedValue : EventEmitter<string> = new EventEmitter<string>()
+
+  masked : any
+  updatedString : string = ''
 
   constructor(private element : ElementRef,
               private renderer: Renderer2) {
 
   }
 
+  ngOnInit() {
+    this.masked = this.renderer.createElement('input')
+    this.renderer.appendChild(this.element.nativeElement.parentElement, this.masked)   
+    this.masked.className = this.element.nativeElement.className
+    this.element.nativeElement.hidden = true
+    this.masked.value = this.element.nativeElement.value
+  }
+  
   ngAfterViewInit() {
-    this.renderer.listen(this.element.nativeElement, KEY_UP, this.eventHandler.bind(this))
+    this.renderer.listen(this.masked, KEY_UP, this.eventHandler.bind(this))
   }
 
   private eventHandler(event : any) {
+
+    let startSkipCount = this.maskingParams.startSkipCount || 0,
+        endSkipCount   = this.maskingParams.endSkipCount   || 0        
+
     if (event.srcElement.value.length > this.updatedString.length) {
       this.updatedString  += JSON.parse(JSON.stringify(event.srcElement.value.substr(event.srcElement.value.length - 1)))
     } else {
@@ -40,16 +61,23 @@ export class MaskingValueDirective {
       }
     }
 
-    if (this.maxLength === this.updatedString.length) {
-      this.value.emit(this.updatedString)
+    if (this.maskingParams.maxLength === this.updatedString.length) {
+      this.element.nativeElement.value = this.updatedString
+      this.maskedValue.emit(this.updatedString)
     }
 
-    if (!this.maskedLength) return
-      
-    if (event.srcElement.value.length <= this.maskedLength) {
-      event.srcElement.value = event.srcElement.value.replace(/\w/g, 'X')
+    if (!this.maskingParams.maskedLength) return
+       
+    const length = event.srcElement.value.length
+    if (length >= startSkipCount && length <= this.maskingParams.maxLength - endSkipCount) {
+      event.srcElement.value = event.srcElement.value.substr(0, length - 1) + this.maskingParams.maskWith   
     }
 
+
+    // Previous code when startSkipCount and endSkipCount was not included in the params
+    // if (event.srcElement.value.length <= this.maskingParams.maskedLength) {
+    //  event.srcElement.value = event.srcElement.value.replace(/\w/g, 'X')      
+    // }
   }
 
 }
