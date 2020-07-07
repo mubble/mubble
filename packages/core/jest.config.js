abd9bbdfae23090ba6857bde0068029f10274b44
Kamal J <kamalakannan@obopay.com>
2020-07-07 17:18:46 +0530

    add jest to 'browser' package

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   displayName             : 'Mubble/Core',
-  "roots"                 : ["<rootDir>/src"],
-  "testMatch"             : [
-                              "**/__tests__/**/*.+(ts|tsx|js)",
-                              "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  roots                   : ['<rootDir>/src'],
+  testMatch               : [
+                              '**/__tests__/**/*.+(ts|tsx|js)',
+                              '**/?(*.)+(spec|test).+(ts|tsx|js)'
                             ],
-  "transform"             : {
-                              "^.+\\.(ts|tsx)$": "ts-jest"
+  transform               : {
+                              '^.+\\.(ts|tsx)$': 'ts-jest'
                             },
   collectCoverage         : true,
   coverageReporters       : ['html'],
